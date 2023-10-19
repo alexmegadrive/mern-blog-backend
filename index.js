9893743e0b90497063aee6e5c00c25f8248a666f
Alexander Mihailov <alexmihailov91@gmail.com>
2023-10-19 18:22:42 +0300

    feat: update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import fs from "fs";
-import multer from "multer";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import cors from "cors";
 import { validationResult } from "express-validator";
-
-import mongoose from "mongoose";
-
 import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
+import fs from "fs";
+import multer from "multer";
 
 const app = express();
 mongoose
   .connect(
-    "mongodb+srv://alex:mJ718spWn4WZj8pN@cluster0.3ryry0y.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://alex:mJ718spWn4WZj8pN@cluster0.3ryry0y.mongodb.net/blog?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("DB OK");
@@ -23,12 +23,24 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
-  res.json({ success: true });
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.email,
+    avatarUrl: req.body.email,
+    passwordHash,
+  });
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(process.env.PORT || 4444, (err) => {
