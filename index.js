b6d180c945afbe42c70acdcd574394aa35bc87c0
Alexander Mihailov <alexmihailov91@gmail.com>
2023-10-20 11:53:05 +0300

    feat: move callbacks to usercontroller

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 import cors from "cors";
-import { validationResult } from "express-validator";
+import checkAuth from "./utils/checkAuth.js";
 import { registerValidation } from "./validations/auth.js";
-import UserModel from "./models/User.js";
-import fs from "fs";
-import multer from "multer";
+import { UserController } from "./controllers/index.js";
 
-const app = express();
 mongoose
   .connect(
     "mongodb+srv://alex:mJ718spWn4WZj8pN@cluster0.3ryry0y.mongodb.net/blog?retryWrites=true&w=majority"
@@ -20,33 +16,17 @@ mongoose
     console.log("DB error");
   });
 
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.email,
-    avatarUrl: req.body.email,
-    passwordHash,
-  });
-  const user = await doc.save();
-
-  res.json(user);
-});
+app.post("/auth/login", UserController.login);
+app.post("/auth/register", registerValidation, UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
-
   console.log("Server OK");
 });
